990baa9ad94043f1691555e0ec07b897fd01f6bc
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2020-01-06 17:00:16 +0800

    fix: 修复点击按钮点击两次，才能粘贴的问题

--- src/rap/mock/index.js
+++ src/rap/mock/index.js
@@ -3,20 +3,18 @@ $(document).on("DOMNodeInserted", ".body", () => {
   $(".badge").remove();
 });
 
-render(".title").then(() => {
+isRender(".Routes").then(() => {
   $("#root").before(`
     <div id="tools">
       <button id="mock" class="btn btn-primary edit">一键复制 Mock</button>
     </div>
   `);
-});
-
-$(document).on("click", "#mock", () => {
-  const api = $("div.InterfaceSummary > ul > li:nth-child(1) > a").text();
-  const response = $("div.result-template.col-6 > pre").text();
-  const result = `"POST ${api}": ${response},`;
 
   new ClipboardJS("#mock", {
-    text: () => result
+    text: () => {
+      const api = $("div.InterfaceSummary > ul > li:nth-child(1) > a").text();
+      const response = $("div.result-template.col-6 > pre").text();
+      return `"POST ${api}": ${response},`;
+    }
   });
 });
